c6f94284557a7322389eb4d9b0413787c9ee4cef
ZhengGang85129 <66740894+ZhengGang85129@users.noreply.github.com>
2025-03-16 12:56:40 +0000

    Deploying to gh-pages from @ ZhengGang85129/ZhengGang85129.github.io@84bd014fcaff4e49634cbcf38df721018fe1f2aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,16 +429,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-received-my-ph-d-in-physics-from-the-department-of-physics-at-national-taiwan-university",
+          title: 'Received my Ph.D. in Physics from the Department of Physics at National Taiwan...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
